28b4d8eb80bc8d2f64520d6e72b219d60aa2f0b0
keeran11 <keeran.sharma11@gmail.com>
2024-04-26 17:22:17 +0545

    this is my first program of python

---
+++ FirstProgram.py
@@ -0,0 +1,14 @@
+print('hello kiran')
+print('This is my firt day of my python challange')
+print(35+5)
+
+# this is the example of variable and its value
+
+name = 'kiran'
+age = '23'
+price = '125.55'
+print(name)
+print (age)
+print(price)
+print('my name is : ', name)
+print('my age is : ',age)
