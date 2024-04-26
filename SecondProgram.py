756fbdde5d8c538f3451181ac9a8002b00ba97f9
keeran11 <keeran.sharma11@gmail.com>
2024-04-26 19:56:34 +0545

    this is my 3rd day of my code

---
+++ SecondProgram.py
@@ -0,0 +1,12 @@
+# this is the program of calculating sum, diff, mul, divide 
+
+a = 6
+b = 6
+sum = a+b
+diff = a-b
+mul = a*b
+divide = a/b
+print ('the sum of the given question is :', sum)
+print('the difference of the given question is : ' , diff)
+print('the multiplication of the given question is : ' , mul)
+print('the division of the given question is : ', divide)
---
+++ TPgm.py
@@ -0,0 +1,64 @@
+# input statement using two input number & print the sum.
+
+a = int(input ('Enter the first number a:', ))
+b = int(input ('Enter the second number b :', ))
+
+sum = (a + b)
+print("The sum is:", sum)
+
+# this is the another program
+
+name = input("Enter the name:", )
+Age = int(input("Enter the Age:",))
+Marks = float(input("Enter the Marks of the Students:",))
+
+
+print("Your name is ", name)
+print("Your age is", Age)
+print("Your Marks of the test is", Marks)
+
+
+# this is third program to input side of the square & print its area.
+
+length= int(input("Enter the length of the squre:",))
+l= length
+Area = (l**2)
+print("The area of the square is :", Area)
+
+
+# wap to input 2 floating point number & print their average.
+
+first = float(input("Enter the first number: ", ))
+second = float(input("Enter the second number: ",))
+
+average = ((first + second)/2)
+print(average)
+
+               
+
+# wap to input 2 int number, a and b.
+# print True if a is greater then or equal to b. if not print False.
+# tala ko code ma spacing le pani kati farak paarxa vanni janauxa so spacing lai ni dyaan dina paaro hai kiran taile.
+
+
+a = int(input("Enter the first number:",))
+b = int(input("Enter the second number:",))
+
+if a >= b:
+
+print("The is true")
+
+else:
+
+print("This is false")
+
+# this is the correct program with correct space.
+
+
+a = int(input("Enter the first number: ", ))
+b = int(input("Enter the second number: ", ))
+
+if a >= b:
+    print("True")
+else:
+    print("False")
